5b6859381efc6ed8b2308866c6b92b70c8139045
Arshia Moghaddam <ars2062@gmail.com>
2024-03-02 13:04:08 +0330

    refactor: tpoint variable usage

--- src/Line.ts
+++ src/Line.ts
@@ -4,30 +4,38 @@ import Point from "./Point";
 export default class Line {
   point1: Point;
   point2: Point;
-  tPoint?: Point;
+  _tPoint: Point;
   p5: p5;
   constructor(p5: p5, point1: Point, point2: Point) {
     this.point1 = point1;
     this.point2 = point2;
     this.p5 = p5;
+    this._tPoint = this.getPointFromT(0);
   }
 
-  getPointFromT(t: number) {
+  private getPointFromT(t: number) {
     const x = this.point1.x + t * (this.point2.x - this.point1.x);
     const y = this.point1.y + t * (this.point2.y - this.point1.y);
-    if (!this.tPoint) this.tPoint = new Point(this.p5, x, y);
-    else {
-      this.tPoint.x = x;
-      this.tPoint.y = y;
-    }
-    return this.tPoint;
+    return new Point(this.p5, x, y);
   }
 
-  draw(t: number) {
-    this.p5.stroke(255);
+  get TPoint(): Point {
+    return this._tPoint;
+  }
+
+  set TPoint(t: number) {
+    const x = this.point1.x + t * (this.point2.x - this.point1.x);
+    const y = this.point1.y + t * (this.point2.y - this.point1.y);
+    this._tPoint.x = x;
+    this._tPoint.y = y;
+  }
+
+  updateTPoints(t: number) {}
+
+  draw(t: number, stroke=255) {
+    this.p5.stroke(stroke);
     this.p5.line(this.point1.x, this.point1.y, this.point2.x, this.point2.y);
-    const tPoint = this.getPointFromT(t);
-    tPoint.draw(0);
-    return tPoint;
+    this.TPoint = t;
+    this.TPoint.draw(0);
   }
 }
